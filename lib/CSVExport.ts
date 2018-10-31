bda402a96d4a0e656554029aab5e96da5ef0d680
Bo Gosmer <bogosmer@gmail.com>
2018-10-31 12:44:27 +0100

    Handling potential value of undefined better a few places

--- lib/CSVExport.ts
+++ lib/CSVExport.ts
@@ -353,23 +353,23 @@ export class CSVExport {
                 // TODO: Consider if we should use ; as field separator instead? I still think we would need to wrap in "" though because of freetext and localized amounts.
 
                 const discountAmount = numeral(0).add(lineItem["retail_price"] || 0).subtract(lineItem["sub_total"] || 0);
-                overrides["discount_amount"] = discountAmount;
+                aggregates["discount_amount"] = discountAmount;
 
                 for (const property of amountProperties) {
-                    if (lineItem[property] !== null) {
+                    if (lineItem[property] != null) {
                         const amount = numeral(0).add(lineItem[property]);
                         const formatted = this.formatNumber(amount.format('0.00'))
                         overrides[property] = `"${formatted}"`
                     }
                 }
                 for (const property of valueProperties) {
-                    if (lineItem[property] !== null) {
+                    if (lineItem[property] != null) {
                         overrides[property] = `"${lineItem[property]}"`
                     }
                 }
 
                 for (const property of localizedProperties) {
-                    if (lineItem[property] !== null) {
+                    if (lineItem[property] != null) {
                         overrides[property] = `"${localize(lineItem[property], "da")}"`
                     }
                 }
