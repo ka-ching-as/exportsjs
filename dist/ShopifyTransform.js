70322ae9f89236b5ee78e76dbc42fbd0c84f1b35
Bo Gosmer <beegee@tutanota.com>
2019-05-16 13:36:05 +0200

    Added sales transform to ShopifyTransform

--- dist/ShopifyTransform.js
+++ dist/ShopifyTransform.js
@@ -9,11 +9,101 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const _ = require("lodash");
+const parsefullname = require("parse-full-name");
 const request = require("request-promise");
+var TaxType;
+(function (TaxType) {
+    TaxType["VAT"] = "vat";
+    TaxType["SALES_TAX"] = "sales_tax";
+})(TaxType || (TaxType = {}));
 class ShopifyTransform {
     constructor(configuration, data) {
         this.data = data;
         this.configuration = configuration;
+    }
+    exportSale() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.validateSalesConfiguration(this.configuration);
+            this.validateSale(this.data);
+            const sale = this.data;
+            const order = {
+                currency: sale.base_currency_code,
+                customer: {
+                    id: Number(sale.customer.id)
+                }
+            };
+            if (this.configuration.tax_type === TaxType.VAT) {
+                order.taxes_included = true;
+            }
+            else {
+                order.taxes_included = false;
+            }
+            if (sale.comment) {
+                order.note_attributes = [{
+                        name: "Basket comment",
+                        value: sale.comment
+                    }];
+            }
+            const shippingLine = this.shippingLines(sale)[0];
+            const shipping = shippingLine.behavior.shipping;
+            const shippingAddress = shipping.address;
+            const shippingCustomerInfo = shipping.customer_info;
+            const parsedName = parsefullname(shippingAddress.name);
+            const shopifyShipping = {};
+            shopifyShipping.first_name = parsedName.first || "";
+            shopifyShipping.last_name = parsedName.last || "";
+            shopifyShipping.address1 = shippingAddress.street;
+            shopifyShipping.city = shippingAddress.city;
+            shopifyShipping.zip = shippingAddress.postal_code;
+            if (shippingAddress.country_code) {
+                shopifyShipping.country_code = shippingAddress.country_code || this.configuration.default_country_code;
+            }
+            shopifyShipping.phone = shippingCustomerInfo.phone;
+            if (shipping.method_id) {
+                const shopifyShippingLine = {
+                    code: shipping.method_id,
+                    price: shippingLine.total,
+                    title: shipping.method_id
+                };
+                const taxes = this.shopifyTaxLines(shippingLine.takes);
+                if (taxes.length > 0) {
+                    shopifyShippingLine.tax_lines = taxes;
+                }
+                order.shipping_lines = [shopifyShippingLine];
+            }
+            order.shipping = shopifyShipping;
+            order.email = shippingCustomerInfo.email;
+            const shopifyLineItems = [];
+            for (const lineItem of this.ecommerceLines(sale)) {
+                let variantId = lineItem.variant_id;
+                if (_.isNil(variantId)) {
+                    const shopifyProduct = yield this.shopifyProduct(lineItem.id, this.configuration);
+                    if (shopifyProduct &&
+                        shopifyProduct.product &&
+                        shopifyProduct.product.variants &&
+                        shopifyProduct.product.variants[0] &&
+                        shopifyProduct.product.variants[0].variant_id) {
+                        variantId = `${shopifyProduct.product.variants[0].variant_id}`;
+                    }
+                }
+                if (!variantId) {
+                    throw new Error(`Couldn't find variant id in Shopify for product id ${lineItem.id}`);
+                }
+                const shopifyLineItem = {
+                    price: lineItem.total,
+                    quantity: lineItem.quantity,
+                    variant_id: Number(variantId)
+                };
+                const taxes = this.shopifyTaxLines(shopifyLineItem.takes);
+                if (taxes.length > 0) {
+                    shopifyLineItem.tax_lines = taxes;
+                }
+                shopifyLineItems.push(shopifyLineItem);
+            }
+            order.line_items = shopifyLineItems;
+            order.financial_status = "paid";
+            return { order: order };
+        });
     }
     exportStockEvent() {
         return __awaiter(this, void 0, void 0, function* () {
@@ -29,38 +119,9 @@ class ShopifyTransform {
             if (_.isNil(productId)) {
                 throw new Error("Missing product id");
             }
-            const base64 = new Buffer(`${this.configuration.api_key}:${this.configuration.password}`).toString("base64");
-            const basicAuthValue = `Basic ${base64}`;
-            const options = {
-                headers: {
-                    Authorization: basicAuthValue
-                },
-                json: true
-            };
-            let inventoryItemId = undefined;
-            const variantId = this.data.variant_id;
-            if (!_.isNil(variantId)) {
-                const url = `https://${this.configuration.shopify_id}.myshopify.com/admin/api/2019-04/variants/${variantId}.json`;
-                const shopifyVariantResult = yield request.get(url, options);
-                if (shopifyVariantResult &&
-                    shopifyVariantResult.variant &&
-                    shopifyVariantResult.variant.inventory_item_id) {
-                    inventoryItemId = `${shopifyVariantResult.variant.inventory_item_id}`;
-                }
-            }
-            else {
-                const url = `https://${this.configuration.shopify_id}.myshopify.com/admin/api/2019-04/products/${productId}.json`;
-                const shopifyProductResult = yield request.get(url, options);
-                if (shopifyProductResult &&
-                    shopifyProductResult.product &&
-                    shopifyProductResult.product.variants &&
-                    shopifyProductResult.product.variants[0] &&
-                    shopifyProductResult.product.variants[0].inventory_item_id) {
-                    inventoryItemId = `${shopifyProductResult.product.variants[0].inventory_item_id}`;
-                }
-            }
+            const inventoryItemId = yield this.inventoryItemId(productId, this.data.variant_id, this.configuration);
             if (_.isNil(inventoryItemId)) {
-                throw new Error(`Failed to find inventory item id from product id ${productId} and variant id ${variantId}`);
+                throw new Error(`Failed to find inventory item id from product id ${productId} and variant id ${this.data.variant_id}`);
             }
             const result = {
                 location_id: locationId,
@@ -80,6 +141,87 @@ class ShopifyTransform {
             return result;
         });
     }
+    inventoryItemId(productId, variantId, configuration) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let inventoryItemId = undefined;
+            if (!_.isNil(variantId)) {
+                const url = `https://${configuration.shopify_id}.myshopify.com/admin/api/2019-04/variants/${variantId}.json`;
+                const shopifyVariantResult = yield request.get(url, this.shopifyRequestOptions(configuration));
+                if (shopifyVariantResult &&
+                    shopifyVariantResult.variant &&
+                    shopifyVariantResult.variant.inventory_item_id) {
+                    inventoryItemId = `${shopifyVariantResult.variant.inventory_item_id}`;
+                }
+            }
+            else {
+                const shopifyProductResult = yield this.shopifyProduct(productId, configuration);
+                if (shopifyProductResult &&
+                    shopifyProductResult.product &&
+                    shopifyProductResult.product.variants &&
+                    shopifyProductResult.product.variants[0] &&
+                    shopifyProductResult.product.variants[0].inventory_item_id) {
+                    inventoryItemId = `${shopifyProductResult.product.variants[0].inventory_item_id}`;
+                }
+            }
+            return inventoryItemId;
+        });
+    }
+    shopifyProduct(productId, configuration) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const url = `https://${configuration.shopify_id}.myshopify.com/admin/api/2019-04/products/${productId}.json`;
+            return yield request.get(url, this.shopifyRequestOptions(configuration));
+        });
+    }
+    shopifyRequestOptions(configuration) {
+        const base64 = new Buffer(`${configuration.api_key}:${configuration.password}`).toString("base64");
+        const basicAuthValue = `Basic ${base64}`;
+        const options = {
+            headers: {
+                Authorization: basicAuthValue
+            },
+            json: true
+        };
+        return options;
+    }
+    shopifyTaxLines(taxes) {
+        const result = (taxes || []).map((tax) => {
+            return {
+                price: tax.amount,
+                rate: tax.rate,
+                title: tax.name
+            };
+        });
+        return result;
+    }
+    ecommerceLines(sale) {
+        return (sale.summary.line_items || []).filter((line) => { return !_.isNil(line.ecom_id); });
+    }
+    shippingLines(sale) {
+        return (sale.summary.line_items || []).filter((line) => {
+            const behavior = line.behavior || {};
+            return !_.isNil(behavior.shipping);
+        });
+    }
+    validateSalesConfiguration(configuration) {
+        if (_.isNil(configuration.api_key) || typeof (configuration.api_key) !== "string") {
+            throw new Error("shopify api key is missing from configuration");
+        }
+        if (_.isNil(configuration.password) || typeof (configuration.password) !== "string") {
+            throw new Error("shopify password is missing from configuration");
+        }
+        if (_.isNil(configuration.shopify_id) || typeof (configuration.shopify_id) !== "string") {
+            throw new Error("shopify shop id is missing from configuration");
+        }
+        if (_.isNil(configuration.tax_type) || typeof (configuration.tax_type) !== "string" || (configuration.tax_type !== TaxType.VAT && configuration.tax_type !== TaxType.SALES_TAX)) {
+            throw new Error("tax_type is invalid in configuration - must be present and either 'vat' or 'sales_tax'");
+        }
+        if (_.isNil(configuration.default_country_code) || typeof (configuration.default_country_code) !== "string") {
+            throw new Error("default_country_code is missing from configuration");
+        }
+        if (_.isNil(configuration.location_id_map) || typeof (configuration.location_id_map) !== "object") {
+            throw new Error("shopify location_id_map is missing from configuration");
+        }
+    }
     validateStockConfiguration(configuration) {
         if (_.isNil(configuration.api_key) || typeof (configuration.api_key) !== "string") {
             throw new Error("shopify api key is missing from configuration");
@@ -92,6 +234,26 @@ class ShopifyTransform {
         }
         if (_.isNil(configuration.location_id_map) || typeof (configuration.location_id_map) !== "object") {
             throw new Error("shopify location_id_map is missing from configuration");
+        }
+    }
+    validateSale(sale) {
+        if (sale.voided || sale.summary.is_return) {
+            throw new Error(`Sale is either voided ${sale.voided} or is return ${sale.is_return}`);
+        }
+        if (!sale.customer || !sale.customer.id || Number(sale.customer.id) === NaN) {
+            throw new Error(`Customer id invalid on sale. Customer: ${JSON.stringify(sale.customer)}`);
+        }
+        const ecomLineItems = this.ecommerceLines(sale);
+        if (ecomLineItems.length === 0) {
+            throw new Error(`No ecommerce line items on sale`);
+        }
+        const ecomLineItemsWithoutProductId = ecomLineItems.filter((line) => { return !_.isNil(line.id); });
+        if (ecomLineItemsWithoutProductId.length !== ecomLineItems.length) {
+            throw new Error(`1 or more ecommerce lines are missing product id`);
+        }
+        const shippingLines = this.shippingLines(sale);
+        if (shippingLines.length !== 1) {
+            throw new Error(`Invalid number of shipping lines on sale ${shippingLines.length}`);
         }
     }
 }
--- lib/ShopifyTransform.ts
+++ lib/ShopifyTransform.ts
@@ -1,5 +1,11 @@
 import * as _ from "lodash"
+import * as parsefullname from "parse-full-name"
 import * as request from "request-promise"
+
+enum TaxType {
+    VAT = "vat",
+    SALES_TAX = "sales_tax"
+}
 
 export class ShopifyTransform {
 
@@ -11,7 +17,123 @@ export class ShopifyTransform {
         this.configuration = configuration
     }
 
-    async exportStockEvent() {
+    // This method takes ecom line items and shipping information
+    // and turns it into a shopify order.
+    // The use case is to use the Shopify webshop for processing
+    // ecommerce orders made in Ka-ching.
+    // In regards to amounts and prices the goal is to have them make
+    // sense with regards to the Ka-ching order and tax wise as well. 
+    // To do that I've used to totals and taxes on the lines as they
+    // are. Information about discounts are not provided to Shopify.
+    // https://help.shopify.com/en/api/reference/orders/order
+    async exportSale(): Promise<any> {
+        // validate configuration
+        this.validateSalesConfiguration(this.configuration)
+        
+        // validate sale
+        this.validateSale(this.data)
+
+        // local sale var
+        const sale = this.data
+
+        // build shopify order
+        const order: any = {
+            currency: sale.base_currency_code,
+            customer: {
+                id: Number(sale.customer.id)
+            }
+        }
+
+        // tax type - Shopify can only handle either vat type or sales tax type, not mixed. 
+        // The taxes_included field specifies whether taxes er included in sub total or not
+        if (this.configuration.tax_type === TaxType.VAT) {
+            order.taxes_included = true
+        } else {
+            order.taxes_included = false
+        }
+
+        // comment
+        if (sale.comment) {
+            order.note_attributes = [{
+                name: "Basket comment",
+                value: sale.comment
+            }]
+        }
+
+        // shipping
+        const shippingLine = this.shippingLines(sale)[0]
+        const shipping = shippingLine.behavior.shipping
+        const shippingAddress = shipping.address
+        const shippingCustomerInfo = shipping.customer_info
+        const parsedName = parsefullname(shippingAddress.name)
+        
+        const shopifyShipping: any = {}
+        shopifyShipping.first_name = parsedName.first || ""
+        shopifyShipping.last_name = parsedName.last || ""
+        shopifyShipping.address1 = shippingAddress.street
+        shopifyShipping.city = shippingAddress.city
+        shopifyShipping.zip = shippingAddress.postal_code
+        if (shippingAddress.country_code) {
+            shopifyShipping.country_code = shippingAddress.country_code || this.configuration.default_country_code
+        }
+        shopifyShipping.phone = shippingCustomerInfo.phone
+        
+        if (shipping.method_id) {
+            const shopifyShippingLine: any = {
+                code: shipping.method_id,
+                price: shippingLine.total,
+                title: shipping.method_id // Required field. Don't have anything better to put here unfortunately...
+            }
+            const taxes = this.shopifyTaxLines(shippingLine.takes)
+            if (taxes.length > 0) {
+                shopifyShippingLine.tax_lines = taxes
+            }
+            order.shipping_lines = [shopifyShippingLine]
+        }
+
+        order.shipping = shopifyShipping
+        order.email = shippingCustomerInfo.email
+
+        // line items
+        const shopifyLineItems: any[] = []
+        for (const lineItem of this.ecommerceLines(sale)) {
+            let variantId: string | undefined = lineItem.variant_id
+            if (_.isNil(variantId)) {
+                const shopifyProduct = await this.shopifyProduct(lineItem.id, this.configuration)
+                if (shopifyProduct && 
+                    shopifyProduct.product && 
+                    shopifyProduct.product.variants && 
+                    shopifyProduct.product.variants[0] && 
+                    shopifyProduct.product.variants[0].variant_id) {
+                    variantId = `${shopifyProduct.product.variants[0].variant_id}`
+                }
+            }
+
+            if (!variantId) {
+                throw new Error(`Couldn't find variant id in Shopify for product id ${lineItem.id}`)
+            }
+            const shopifyLineItem: any = {
+                price: lineItem.total,
+                quantity: lineItem.quantity,
+                variant_id: Number(variantId)
+            }
+
+            const taxes = this.shopifyTaxLines(shopifyLineItem.takes)
+            if (taxes.length > 0) {
+                shopifyLineItem.tax_lines = taxes
+            }
+
+            shopifyLineItems.push(shopifyLineItem)
+        }
+        order.line_items = shopifyLineItems
+
+        // transactions
+        order.financial_status = "paid"
+
+        return { order: order }
+    }
+
+    async exportStockEvent(): Promise<any> {
         // validate configuration
         this.validateStockConfiguration(this.configuration)
 
@@ -32,38 +154,9 @@ export class ShopifyTransform {
         }
 
         // lookup inventory item id in shopify
-        const base64 = new Buffer(`${this.configuration.api_key}:${this.configuration.password}`).toString("base64")
-        const basicAuthValue = `Basic ${base64}`
-        const options: request.RequestPromiseOptions = {
-            headers: {
-                Authorization: basicAuthValue
-            },
-            json: true
-        }
-        let inventoryItemId: string | undefined = undefined
-        const variantId = this.data.variant_id
-        if (!_.isNil(variantId)) {
-            const url = `https://${this.configuration.shopify_id}.myshopify.com/admin/api/2019-04/variants/${variantId}.json`
-            const shopifyVariantResult = await request.get(url, options)
-            if (shopifyVariantResult && 
-                shopifyVariantResult.variant && 
-                shopifyVariantResult.variant.inventory_item_id) {
-                inventoryItemId = `${shopifyVariantResult.variant.inventory_item_id}`
-            }
-        } else {
-            const url = `https://${this.configuration.shopify_id}.myshopify.com/admin/api/2019-04/products/${productId}.json`
-            const shopifyProductResult = await request.get(url, options)
-            if (shopifyProductResult && 
-                shopifyProductResult.product && 
-                shopifyProductResult.product.variants && 
-                shopifyProductResult.product.variants[0] && 
-                shopifyProductResult.product.variants[0].inventory_item_id) {
-                inventoryItemId = `${shopifyProductResult.product.variants[0].inventory_item_id}`
-            }
-        }
-
+        const inventoryItemId = await this.inventoryItemId(productId, this.data.variant_id, this.configuration)
         if (_.isNil(inventoryItemId)) {
-            throw new Error(`Failed to find inventory item id from product id ${productId} and variant id ${variantId}`)
+            throw new Error(`Failed to find inventory item id from product id ${productId} and variant id ${this.data.variant_id}`)
         }
         
         // build and return result
@@ -85,6 +178,94 @@ export class ShopifyTransform {
         return result
     }
 
+    private async inventoryItemId(productId: string, variantId: string | undefined, configuration: any): Promise<string | undefined> {
+        let inventoryItemId: string | undefined = undefined
+        if (!_.isNil(variantId)) {
+            const url = `https://${configuration.shopify_id}.myshopify.com/admin/api/2019-04/variants/${variantId}.json`
+            const shopifyVariantResult = await request.get(url, this.shopifyRequestOptions(configuration))
+            if (shopifyVariantResult && 
+                shopifyVariantResult.variant && 
+                shopifyVariantResult.variant.inventory_item_id) {
+                inventoryItemId = `${shopifyVariantResult.variant.inventory_item_id}`
+            }
+        } else {
+            const shopifyProductResult = await this.shopifyProduct(productId, configuration)
+            if (shopifyProductResult && 
+                shopifyProductResult.product && 
+                shopifyProductResult.product.variants && 
+                shopifyProductResult.product.variants[0] && 
+                shopifyProductResult.product.variants[0].inventory_item_id) {
+                inventoryItemId = `${shopifyProductResult.product.variants[0].inventory_item_id}`
+            }
+        }
+        return inventoryItemId
+    }
+
+    private async shopifyProduct(productId: string, configuration: any): Promise<any> {
+        const url = `https://${configuration.shopify_id}.myshopify.com/admin/api/2019-04/products/${productId}.json`
+        return await request.get(url, this.shopifyRequestOptions(configuration))
+    }
+
+    private shopifyRequestOptions(configuration: any): request.RequestPromiseOptions {
+        const base64 = new Buffer(`${configuration.api_key}:${configuration.password}`).toString("base64")
+        const basicAuthValue = `Basic ${base64}`
+        const options: request.RequestPromiseOptions = {
+            headers: {
+                Authorization: basicAuthValue
+            },
+            json: true
+        }
+        return options
+    }
+
+    private shopifyTaxLines(taxes: any[]): any[] {
+        const result = (taxes || []).map((tax: any) => {
+            return {
+                price: tax.amount,
+                rate: tax.rate,
+                title: tax.name
+            }
+        })
+        return result
+    }
+
+    private ecommerceLines(sale: any): any[] {
+        return (sale.summary.line_items || []).filter((line: any)=> { return !_.isNil(line.ecom_id) })
+    }
+
+    private shippingLines(sale: any): any[] {
+        return (sale.summary.line_items || []).filter((line: any)=> { 
+            const behavior = line.behavior || {}
+            return !_.isNil(behavior.shipping)
+        })
+    }
+
+    private validateSalesConfiguration(configuration: any) {
+        if (_.isNil(configuration.api_key) || typeof(configuration.api_key) !== "string") {
+            throw new Error("shopify api key is missing from configuration")
+        }
+
+        if (_.isNil(configuration.password) || typeof(configuration.password) !== "string") {
+            throw new Error("shopify password is missing from configuration")
+        }
+
+        if (_.isNil(configuration.shopify_id) || typeof(configuration.shopify_id) !== "string") {
+            throw new Error("shopify shop id is missing from configuration")
+        }
+
+        if (_.isNil(configuration.tax_type) || typeof(configuration.tax_type) !== "string" || (configuration.tax_type !== TaxType.VAT && configuration.tax_type !== TaxType.SALES_TAX)) {
+            throw new Error("tax_type is invalid in configuration - must be present and either 'vat' or 'sales_tax'")
+        }
+
+        if (_.isNil(configuration.default_country_code) || typeof(configuration.default_country_code) !== "string") {
+            throw new Error("default_country_code is missing from configuration")
+        }
+
+        if (_.isNil(configuration.location_id_map) || typeof(configuration.location_id_map) !== "object") {
+            throw new Error("shopify location_id_map is missing from configuration")
+        }
+    }
+
     private validateStockConfiguration(configuration: any) {
         if (_.isNil(configuration.api_key) || typeof(configuration.api_key) !== "string") {
             throw new Error("shopify api key is missing from configuration")
@@ -100,6 +281,34 @@ export class ShopifyTransform {
 
         if (_.isNil(configuration.location_id_map) || typeof(configuration.location_id_map) !== "object") {
             throw new Error("shopify location_id_map is missing from configuration")
+        }
+    }
+
+    private validateSale(sale: any) {
+        // returns and voided are not handled here
+        if (sale.voided || sale.summary.is_return) {
+            throw new Error(`Sale is either voided ${sale.voided} or is return ${sale.is_return}`)
+        }
+
+        // ensure customer id
+        if (!sale.customer || !sale.customer.id || Number(sale.customer.id) === NaN) {
+            throw new Error(`Customer id invalid on sale. Customer: ${JSON.stringify(sale.customer)}`)
+        }
+
+        // ensure ecom lines
+        const ecomLineItems = this.ecommerceLines(sale)
+        if (ecomLineItems.length === 0) {
+            throw new Error(`No ecommerce line items on sale`)
+        }
+        const ecomLineItemsWithoutProductId = ecomLineItems.filter((line: any) => { return !_.isNil(line.id) })
+        if (ecomLineItemsWithoutProductId.length !== ecomLineItems.length) {
+            throw new Error(`1 or more ecommerce lines are missing product id`)
+        }
+        
+        // ensure exactly 1 shipping line
+        const shippingLines = this.shippingLines(sale)
+        if (shippingLines.length !== 1) {
+            throw new Error(`Invalid number of shipping lines on sale ${shippingLines.length}`)
         }
     }
 }
