6df94c3ee50dc7b6589b91ebfc001c30eec0963d
Bo Gosmer <beegee@tutanota.com>
2019-05-16 13:46:12 +0200

    Added "declaration": true to tsconfig and removed "allowJs": true.

--- dist/ShopifyTransform.d.ts
+++ dist/ShopifyTransform.d.ts
@@ -2,6 +2,15 @@ export declare class ShopifyTransform {
     data: any;
     configuration: any;
     constructor(configuration: any, data: any);
+    exportSale(): Promise<any>;
     exportStockEvent(): Promise<any>;
+    private inventoryItemId;
+    private shopifyProduct;
+    private shopifyRequestOptions;
+    private shopifyTaxLines;
+    private ecommerceLines;
+    private shippingLines;
+    private validateSalesConfiguration;
     private validateStockConfiguration;
+    private validateSale;
 }
